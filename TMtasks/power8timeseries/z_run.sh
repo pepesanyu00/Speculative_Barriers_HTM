4a264d2b9e239c83cf28975f82acb4c078d380c7
pepesanyu00 <i82sayuj@uco.es>
2023-11-10 11:06:54 +0100

    servidor

--- TMtasks/power8timeseries/z_run.sh
+++ TMtasks/power8timeseries/z_run.sh
@@ -8,40 +8,25 @@
 export OMP_PLACES=cores
 
 benchs=("./timeseries/power-MPIII-SVF_n180000.txt 1325"
-        "./timeseries/seismology-MPIII-SVE_n180000.txt 50"
-        "./timeseries/e0103_n180000.txt 500"
-        "./timeseries/penguin_sample_TutorialMPweb.txt 800"
-       )
+        #"./timeseries/seismology-MPIII-SVE_n180000.txt 50"
+        #"./timeseries/e0103_n180000.txt 500"
+        #"./timeseries/penguin_sample_TutorialMPweb.txt 800"
+       #)
  
-#        "./timeseries/audio-MPIII-SVD.txt 200"
-#        "./timeseries/human_activity-MPIII-SVC.txt 120")
-hilos="1 2 4 8 16 32 64 128"
-n=4
+        "./timeseries/audio-MPIII-SVD.txt 200"
+        "./timeseries/human_activity-MPIII-SVC.txt 120")
+hilos="1"
+n=2
 #RIC una tirada con dump stats a 1. Se pone a 0 para las siguientes
 dumpStats=1
 for (( j=0; j<$n; j++ )); do
     for i in "${benchs[@]}"; do
         for t in $hilos; do
             echo "## $j ## $i $t"
-            ./scamp $i $t $dumpStats
-            ./scampTM $i $t 32 $dumpStats
-            ./scampFGL $i $t $dumpStats
-            ./scampTiles $i 128 $t $dumpStats
-            ./scampTiles $i 256 $t $dumpStats
-            ./scampTiles $i 512 $t $dumpStats
-            ./scampTiles $i 1024 $t $dumpStats
-            ./scampTilesTM $i 128 32 $t $dumpStats
-            ./scampTilesTM $i 256 32 $t $dumpStats
-            ./scampTilesTM $i 512 32 $t $dumpStats
-            ./scampTilesTM $i 1024 32 $t $dumpStats
             ./scampTilesDiag $i 128 $t $dumpStats
             ./scampTilesDiag $i 256 $t $dumpStats
             ./scampTilesDiag $i 512 $t $dumpStats
             ./scampTilesDiag $i 1024 $t $dumpStats
-            ./scampTilesUnprot $i 128 $t $dumpStats
-            ./scampTilesUnprot $i 256 $t $dumpStats
-            ./scampTilesUnprot $i 512 $t $dumpStats
-            ./scampTilesUnprot $i 1024 $t $dumpStats
         done;
     done;
     dumpStats=0
