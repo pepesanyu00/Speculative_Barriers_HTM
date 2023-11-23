c7a0421a69fc1f9c092ab757afcc0839d61c182d
pepesanyu00 <i82sayuj@uco.es>
2023-11-23 12:48:47 +0100

    23 nov

--- places/z_run.sh
+++ places/z_run.sh
@@ -6,7 +6,7 @@
 # cores: Each place contains a core. If OMP_PLACES is not set, the default setting is cores.
 # num_places: Is the number of places.
 #export OMP_PLACES="{0:10:4},{10:10:4}"
-export OMP_PLACES=sockets
+export OMP_PLACES="{0,1}:20:8"
 
 #Series cortas
 benchs=(#"./timeseries/power-MPIII-SVF_n180000.txt 1325"
