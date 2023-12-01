878946cd3e742421459ad6f5289ce43dd59afe13
pepesanyu00 <i82sayuj@uco.es>
2023-12-01 13:46:23 +0100

    1 dic

--- specscamptilesdiag/z_run.sh
+++ specscamptilesdiag/z_run.sh
@@ -8,11 +8,11 @@
 export OMP_PLACES=cores
 
 #Series cortas
-benchs=(#"./timeseries/power-MPIII-SVF_n180000.txt 1325"
-    "./timeseries/seismology-MPIII-SVE_n180000.txt 50"
+#benchs=(#"./timeseries/power-MPIII-SVF_n180000.txt 1325"
+#    "./timeseries/seismology-MPIII-SVE_n180000.txt 50"
 #    "./timeseries/e0103_n180000.txt 500"
 #    "./timeseries/penguin_sample_TutorialMPweb.txt 800"
-)
+#)
         #"./timeseries/audio-MPIII-SVD.txt 200"
         #"./timeseries/human_activity-MPIII-SVC.txt 120")
 
@@ -20,7 +20,7 @@ benchs=(#"./timeseries/power-MPIII-SVF_n180000.txt 1325"
 
 #Series largas: 1.8M de muestras
 # "./timeseries/e0103.txt 500"
-#benchs=("./timeseries/power-MPIII-SVF.txt 1325" "./timeseries/seismology-MPIII-SVE.txt 50")
+benchs=("./timeseries/seismology-MPIII-SVE.txt 50")
 
 hilos="1 2 4 8 16 32 64 128"
 n=2
