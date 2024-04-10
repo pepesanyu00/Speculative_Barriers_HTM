6379b209acbe9a8a706a63dd644b12aa10ab9ce5
pepesanyu00 <i82sayuj@uco.es>
2024-04-10 13:23:41 +0200

    debug

--- specIntel/figs/zgen_plots_large.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./plotScampSpeedupLarge.py e0103 500 1
-./plotScampSpeedupLarge.py power-MPIII-SVF 1325 0
-./plotScampSpeedupLarge.py seismology-MPIII-SVE 50 0
-
--- specIntel/figs/zgen_tilesTM_sens_plots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./plotScampTilesTMSens.py audio-MPIII-SVD 200 1 
-./plotScampTilesTMSens.py power-MPIII-SVF_n180000 1325 0
-./plotScampTilesTMSens.py seismology-MPIII-SVE_n180000 50 0
-./plotScampTilesTMSens.py e0103_n180000.txt 500 0
-./plotScampTilesTMSens.py penguin_sample_TutorialMPweb.txt 800 0
-./plotScampTilesTMSens.py human_activity-MPIII-SVC.txt 120 0
