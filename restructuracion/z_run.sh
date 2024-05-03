8ad9c704fa00c44b4b42be10084f22ca593643be
pepe <i82sayuj@uco.es>
2024-05-03 11:42:51 +0200

    resultados e103 40k

--- restructuracion/z_run.sh
+++ restructuracion/z_run.sh
@@ -10,8 +10,8 @@
 
 #Series cortas
 benchs=(#"./timeseries/power-MPIII-SVF_n180000.txt 1325"
-    "./timeseries/seismology-MPIII-SVE_n180000.txt 50"
-#    "./timeseries/e0103_n180000.txt 500"
+#    "./timeseries/seismology-MPIII-SVE_n180000.txt 50"
+    "./timeseries/e0103_n40000.txt 500"
 #    "./timeseries/penguin_sample_TutorialMPweb.txt 800"
 #        "./timeseries/audio-MPIII-SVD.txt 200"
 #       "./timeseries/human_activity-MPIII-SVC.txt 120"
