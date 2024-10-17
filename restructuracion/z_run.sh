37811cb98ad0dd79b36340fc2e69bed2e222db1e
pepe <i82sayuj@uco.es>
2024-10-17 14:06:29 +0200

    resultados audio vuelta

--- restructuracion/z_run.sh
+++ restructuracion/z_run.sh
@@ -13,12 +13,12 @@ benchs=(#"./timeseries/power-MPIII-SVF_n180000.txt 1325"
     #"./timeseries/seismology-MPIII-SVE_n180000.txt 50"
     #"./timeseries/e0103_n180000.txt 500"
     #"./timeseries/penguin_sample_TutorialMPweb.txt 800"
-        #"./timeseries/audio-MPIII-SVD.txt 200"
+        "./timeseries/audio-MPIII-SVD.txt 50"
        #"./timeseries/human_activity-MPIII-SVC.txt 120"
 #        series XL
-        "./timeseries/power-MPIII-SVF.txt 1325"
-        "./timeseries/e0103.txt 500"
-        "./timeseries/seismology-MPIII-SVE.txt 50"
+#        "./timeseries/power-MPIII-SVF.txt 1325"
+#        "./timeseries/e0103.txt 500"
+#        "./timeseries/seismology-MPIII-SVE.txt 50"
 )
 
 
@@ -35,15 +35,15 @@ for (( j=0; j<$n; j++ )); do
             ./specScampTilesDiag $i 512 $t $dumpStats
             ./specScampTilesDiag $i 2048 $t $dumpStats
             ./specScampTilesDiag $i 8192 $t $dumpStats
-	        #./scampTilesDiag $i 128 $t $dumpStats
-            #./scampTilesDiag $i 512 $t $dumpStats
-            #./scampTilesDiag $i 2048 $t $dumpStats
-            #./scampTilesDiag $i 8192 $t $dumpStats
-            #./scamp $i $t $dumpStats
-            #./scampTilesUnprot $i 128 $t $dumpStats
-            #./scampTilesUnprot $i 512 $t $dumpStats
-            #./scampTilesUnprot $i 2048 $t $dumpStats
-            #./scampTilesUnprot $i 8192 $t $dumpStats
+	        ./scampTilesDiag $i 128 $t $dumpStats
+            ./scampTilesDiag $i 512 $t $dumpStats
+            ./scampTilesDiag $i 2048 $t $dumpStats
+            ./scampTilesDiag $i 8192 $t $dumpStats
+            ./scamp $i $t $dumpStats
+            ./scampTilesUnprot $i 128 $t $dumpStats
+            ./scampTilesUnprot $i 512 $t $dumpStats
+            ./scampTilesUnprot $i 2048 $t $dumpStats
+            ./scampTilesUnprot $i 8192 $t $dumpStats
         done;
     done;
     dumpStats=0
